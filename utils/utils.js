31c841ba8bc98721ebd37e3ab3fc8bfd79807c98
Carlos Augusto Calage <calage.carlos@gmail.com>
2022-05-06 23:17:25 -0300

    refactor: matrix calculations function

--- utils/utils.js
+++ utils/utils.js
@@ -2,14 +2,19 @@ const degToRad = d => (d * Math.PI) / 180
 
 const radToDeg = r => (r * 180) / Math.PI
 
-const computeMatrix = (viewProjectionMatrix, rotate, translate, scale) => {
-  let tlt = m4.translate(viewProjectionMatrix, translate[0], translate[1], translate[2])
+const computeMatrix = (viewProjectionMatrix, transformations) => {
+  let tlt = m4.translate(
+    viewProjectionMatrix,
+    transformations.translationX,
+    transformations.translationY,
+    transformations.translationZ
+  )
 
-  let rtx = m4.xRotate(tlt, rotate[0])
-  let rty = m4.yRotate(rtx, rotate[1])
-  let rtz = m4.zRotate(rty, rotate[2])
+  let rtx = m4.xRotate(tlt, transformations.rotationX)
+  let rty = m4.yRotate(rtx, transformations.rotationY)
+  let rtz = m4.zRotate(rty, transformations.rotationZ)
 
-  let matrix = m4.scale(rtz, scale[0], scale[1], scale[2])
+  let matrix = m4.scale(rtz, transformations.scaleX, transformations.scaleY, transformations.scaleZ)
 
   return matrix
 }
