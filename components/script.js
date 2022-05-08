6e0d5ba8bd2d63be1de1b1d8c7406cd1c0f5c787
Carlos Augusto Calage <calage.carlos@gmail.com>
2022-05-07 23:25:23 -0300

    feat: added camera controls

--- components/script.js
+++ components/script.js
@@ -1,14 +1,27 @@
 function main() {
+  /* Initialization */
   const { gl, meshProgramInfo } = initializeWorld()
 
-  // Tell the twgl to match position with a_position, n
-  // normal with a_normal etc..
   twgl.setAttributePrefix('a_')
 
-  // setup GLSL program
-  var programInfo = twgl.createProgramInfo(gl, [vs, fs])
+  let programInfo = twgl.createProgramInfo(gl, [vs, fs])
 
-  var fieldOfViewRadians = degToRad(60)
+  /* Camera Variables */
+
+  let fieldOfViewRadians = {
+    zoom: degToRad(60)
+  }
+
+  let camera_config = {
+    position_x: 0,
+    position_y: 0,
+    position_z: 100,
+    lookat_x: 0,
+    lookat_y: 0,
+    lookat_z: 0
+  }
+
+  /* GUI Variables */
 
   let gui = new GUI()
 
@@ -22,37 +35,41 @@ function main() {
 
   gui.add_button(false, onClick, 'New Polygon')
 
+  gui.add_category('Camera')
+  gui.add_element('Camera', camera_config)
+  gui.custom_element('Camera', fieldOfViewRadians, 'zoom', [degToRad(0), degToRad(90), 0.01])
+
+  /* Scene loop */
+
   requestAnimationFrame(drawScene)
 
-  // Draw the scene.
   function drawScene(time) {
-    time = time * 0.0005
-
     twgl.resizeCanvasToDisplaySize(gl.canvas)
 
-    // Tell WebGL how to convert from clip space to pixels
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
     gl.enable(gl.CULL_FACE)
     gl.enable(gl.DEPTH_TEST)
 
-    // Compute the projection matrix
-    var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight
-    var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, 1, 2000)
+    let aspect = gl.canvas.clientWidth / gl.canvas.clientHeight
+    let projectionMatrix = m4.perspective(fieldOfViewRadians.zoom, aspect, 1, 2000)
 
     // Compute the camera's matrix using look at.
-    var cameraPosition = [0, 0, 100]
-    var target = [0, 0, 0]
-    var up = [0, 1, 0]
-    var cameraMatrix = m4.lookAt(cameraPosition, target, up)
+    let cameraPosition = [
+      camera_config.position_x,
+      camera_config.position_y,
+      camera_config.position_z
+    ]
+    let target = [camera_config.lookat_x, camera_config.lookat_y, camera_config.lookat_z]
+
+    let up = [0, 1, 0]
+
+    let cameraMatrix = m4.lookAt(cameraPosition, target, up)
 
     // Make a view matrix from the camera matrix.
-    var viewMatrix = m4.inverse(cameraMatrix)
+    let viewMatrix = m4.inverse(cameraMatrix)
 
-    var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix)
-
-    var coneXRotation = time
-    var coneYRotation = -time
+    let viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix)
 
     gl.useProgram(programInfo.program)
 
